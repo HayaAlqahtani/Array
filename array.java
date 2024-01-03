c922767f4afddf3019af1e69651c3a53df185e71
Haya S A M A Alqahtani <83316@kfh.com>
2024-01-03 10:12:06 +0300

    commit

---
+++ array.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+      double[] Temp = {25, 30, 35, 40, 45};
+      Temp[2]=15;
+      
+    for(double weather: Temp){
+      System.out.println(weather);
+  
+    }
+    }
